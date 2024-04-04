c0b5c44cf3362c669efeff706473cd135a53aafc
Jothipriya_R <114628306+R-Jothipriya@users.noreply.github.com>
2024-04-04 12:10:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Calculator from './Calcy';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <Calculator/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ Calcy.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+const Calculator = () => {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleInput = (value) => {
+    setInput(input + value);
+  };
+
+  const handleClear = () => {
+    setInput('');
+    setResult('');
+  };
+
+  const handleCalculate = () => {
+    try {
+      setResult(eval(input).toString());
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <input type="text" value={input} readOnly />
+      <div>
+        <button onClick={() => handleInput('1')}>1</button>
+        <button onClick={() => handleInput('2')}>2</button>
+        <button onClick={() => handleInput('3')}>3</button>
+        <button onClick={() => handleInput('+')}>+</button>
+      </div>
+      <div>
+        <button onClick={() => handleInput('4')}>4</button>
+        <button onClick={() => handleInput('5')}>5</button>
+        <button onClick={() => handleInput('6')}>6</button>
+        <button onClick={() => handleInput('-')}>-</button>
+      </div>
+      <div>
+        <button onClick={() => handleInput('7')}>7</button>
+        <button onClick={() => handleInput('8')}>8</button>
+        <button onClick={() => handleInput('9')}>9</button>
+        <button onClick={() => handleInput('*')}>*</button>
+      </div>
+      <div>
+        <button onClick={handleClear}>C</button>
+        <button onClick={() => handleInput('0')}>0</button>
+        <button onClick={handleCalculate}>=</button>
+        <button onClick={() => handleInput('/')}>/</button>
+      </div>
+      <div>
+        <span>Result: {result}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
